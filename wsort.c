4077958e6bb94b40da47d2269c025e5c2b6f89ce
der-birk <94652901+der-birk@users.noreply.github.com>
2022-05-12 23:57:54 +0200

    Add files via upload

---
+++ wsort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct ListNode {
+char* word;
+struct ListNode* next;
+} node;
+
+node* listHead = NULL;
+
+
+//fügt das Wort auf das "letter" zeigt, an den richtigen Platz in der Liste ein
+static int insertElement(char* letter) {
+	
+}
+
+
+int main (int argc, char* argv[]) {
+	
+	//einlesen der Wörter
+    fgets(str, 101, stdin);
+	
+	//Fehlerbehandelung
+	if(str == NULL){
+		
+	}
+    else{
+		insertElement(str);
+	}
+
+	exit(EXIT_SUCCESS);
+}
